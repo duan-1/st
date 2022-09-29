482ce0093f9a5ec32c4061ddf1429b3ec994af27
patoria <patoria11@gmail.com>
2022-09-29 19:43:01 +0800

    feat: changed font to JetbrainsMono

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "WenQuanYi Micro Hei Mono:pixelsize=14:antialias=true:autohint=true";
+static char *font = "JetBrains Mono:pixelsize=14:antialias=true:autohint=true:style=Regular";
 static int borderpx = 2;
 
 /*
